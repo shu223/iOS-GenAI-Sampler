1b14d4ea2667d4805e0e4352ff89233c0635200f
shu223 <shuichi0526@gmail.com>
2024-06-06 17:08:16 +0900

    Update VideoUtils

--- iOS-GenAI-Sampler/VideoUtils.swift
+++ iOS-GenAI-Sampler/VideoUtils.swift
@@ -13,7 +13,7 @@ class VideoUtils {
         // 動画の総時間を取得し、timeInterval秒ごとのCMTimeを生成
         let durationSeconds = try Int(CMTimeGetSeconds(await avAsset.load(.duration)))
         let frameCount = Int(Double(durationSeconds) / timeInterval)
-        let times = (0 ..< frameCount).map { CMTime(seconds: Double($0) * timeInterval, preferredTimescale: 1) }
+        let times = (0 ..< frameCount).map { CMTime(seconds: Double($0) * timeInterval, preferredTimescale: CMTimeScale.max) }
 
         print("duration: \(durationSeconds), frame count: \(times.count)")
 
@@ -55,6 +55,29 @@ public extension AVAsset {
                     print("All frames were extracted.")
                     continuation.resume(returning: cgImages)
                 }
+            }
+        }
+    }
+
+    // 動画のAVAssetからフレームを抽出する
+    func extractFrame(for timestamp: TimeInterval, maximumSize: CGSize = .zero) async -> CGImage? {
+        let time = CMTime(seconds: timestamp, preferredTimescale: CMTimeScale.max)
+        // FIXME: 共通化する
+        let generator = AVAssetImageGenerator(asset: self)
+        generator.appliesPreferredTrackTransform = true
+        generator.requestedTimeToleranceBefore = CMTime.zero
+        generator.requestedTimeToleranceAfter = CMTime.zero
+        generator.maximumSize = maximumSize
+
+        return await withCheckedContinuation { continuation in
+            generator.generateCGImageAsynchronously(for: time) { cgImage, actualTime, error in
+                if let error = error {
+                    print("Error at time \(time.seconds), \(actualTime.seconds): \(error.localizedDescription)")
+                    continuation.resume(returning: nil)
+                    return
+                }
+                print("Image extracted. Requested: \(time.seconds), Actual: \(actualTime.seconds)")
+                continuation.resume(returning: cgImage)
             }
         }
     }
