272f879458f1e8da153a2fe05734d4b76551dad1
shu223 <shuichi0526@gmail.com>
2024-07-21 16:02:19 +0900

    Create StableDiffusionView

---
+++ iOS-GenAI-Sampler/Samples/Stable Diffusion/StableDiffusionView.swift
@@ -0,0 +1,28 @@
+//
+//  StableDiffusionView.swift
+//  iOS-GenAI-Sampler
+//
+//  Created by Shuichi Tsutsumi on 2024/07/21.
+//
+
+import SwiftUI
+import StableDiffusion
+
+struct StableDiffusionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StableDiffusionView()
+}
+
+class StableDiffusionState {
+
+//    func hoge(resourceURL: URL, prompt: String, seed: Int) {
+//        let pipeline = try! StableDiffusionPipeline(resourcesAt: resourceURL, controlNet: [])
+//        try! pipeline.loadResources()
+//        let image = try pipeline.generateImages(prompt: prompt, seed: seed).first
+//    }
+}
